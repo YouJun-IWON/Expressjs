ea8ae58fa0f63311e5245fb19d23e7d04944ef57
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-09-04 20:13:27 +0900

    connect mongodb

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const PORT = 4000;
 
+const productsRouter = require('./routes/products.router');
 const usersRouter = require('./routes/users.router');
 const postsRouter = require('./routes/posts.router');
+const { default: mongoose } = require('mongoose');
 
 const Users = [
   {
@@ -32,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+app.use('/products', productsRouter);
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
@@ -41,8 +44,23 @@ app.listen(PORT, () => {
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    imageTitle: 'wow',
+app.get('/', (req, res, next) => {
+  setImmediate(() => {
+    next(new Error('it is an error'));
   });
+  // res.render('index', {
+  //   imageTitle: 'wow',
+  // });
 });
+
+//! handle async error
+app.use((error, req, res, next) => {
+  res.json({ message: error.message });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://tndhworl1998:12341234@cluster0.gbjaff4.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log('mongodb connected'))
+  .catch((err) => console.log(err));
